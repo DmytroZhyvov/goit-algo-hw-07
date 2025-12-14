20b0ee3506b68d0fa8d3e196c483b5772e791e39
DmytroZhyvov <48692941+DmytroZhyvov@users.noreply.github.com>
2025-12-14 14:11:45 +0100

    Add task no.3

---
+++ task_03.py
@@ -0,0 +1,98 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+
+def find_max_value(root):
+    """
+    Find the maximum value in a binary search tree.
+    """
+    if root is None:
+        return None
+
+    current = root
+    while current.right:
+        current = current.right
+
+    return current.val
+
+
+def find_min_value(root):
+    """
+    Find the minimum value in a binary search tree.
+    """
+    if root is None:
+        return None
+
+    current = root
+    while current.left:
+        current = current.left
+
+    return current.val
+
+
+def sum_tree_values(root):
+    """
+    Find the sum of all values in a binary search tree.
+    """
+    if root is None:
+        return 0
+    return root.val + sum_tree_values(root.left) + sum_tree_values(root.right)
